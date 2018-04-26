fccf8fed80fdfe81271b477dd8342e93c20a091a
Suhail Ahamed <suhailahamed@Suhails-MacBook-Pro.local>
2018-04-26 10:45:20 +0400

    generate food and lengthen snake when it eats it

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 //Suhail's First p5 Project
 
-var myWidth = 600, myHeight = 400;
+var myWidth = 500, myHeight = 500;
 let s1 = [];
 
 //Game block size (pixel)
@@ -10,13 +10,11 @@ blockSize = 10;
 let gameWidth = myWidth / blockSize;
 let gameHeight = myHeight / blockSize;
 
-//Game speed controller, frames per move(Lower = faster)
+//Game speed controller, frames per second
 let gameSpeed = 5;
-let checkLoop = 0;
 
 
 function keyPressed(){
-
 
 	if(key == 'W' && s1[0].down == false){
 		s1[0].up = true;
@@ -50,13 +48,10 @@ function setup() {
 
 function draw() {
 
-	if(checkLoop >gameSpeed){
 		background(24,49,68);
 		for(let i=0;i<(s1.length);i++){
 			s1[i].updateLoc();
 			s1[i].draw();
 		}
-		checkLoop = 0;
-	}
-	checkLoop++;
+		frameRate(gameSpeed)
 }
--- snake.js
+++ snake.js
@@ -2,13 +2,11 @@ class snake{
 	constructor(){
 		this.headX=gameWidth/2,this.headY=gameHeight/2;
 		this.snakeBody = [];
+
+		//Create its body with 7 blocks for now
 		this.snakeBody.push( new body(this.headX,this.headY) );
 		this.snakeBody.push( new body(this.headX,this.headY+1) );
 		this.snakeBody.push( new body(this.headX,this.headY+2) );
-		this.snakeBody.push( new body(this.headX,this.headY+3) );
-		this.snakeBody.push( new body(this.headX,this.headY+4) );
-		this.snakeBody.push( new body(this.headX,this.headY+5) );
-		this.snakeBody.push( new body(this.headX,this.headY+6) );
 
 		//Which way is the snake moving, Initialized to move up
 		this.up = true;
@@ -18,35 +16,49 @@ class snake{
 
 		//Dead or alive?
 		this.alive = true;
+
+		//The food for this snake
+		this.food = new body(floor(random(gameWidth)),floor(random(gameHeight)));
+		//Position food not on snake's body
+		this.positionFood();
+
+		//To check if it ate the food
+		this.foodEaten = false;
 	}
 	draw(){
 		//Rest of the body
 		stroke(0,200,200);
-		fill(0);
-		for(let i=1;i<this.snakeBody.length;i++){
+		fill(0,0,255)
+		if(this.alive)
+			fill(0);
+		for(let i=1;i<this.snakeBody.length;i++)
 			this.snakeBody[i].draw();
-		}
 
 		//Give head different colour
-		fill(200,0,200);
+		if(this.alive)
+			fill(200,0,200);
 		stroke(0,200,200);
 		this.snakeBody[0].draw();
 
+		//draw the food
+		fill(0,200,0);
+		stroke(0,200,0);
+		this.food.draw();
+
 	}
 	updateLoc(){
-		let b = this.snakeBody[0];
+		let b;
 
 		//Dead if it hit the edge
-		if( (this.headY == gameHeight-1 && this.down == true) || (this.headY == 0 && this.up == true) || (this.headX == gameWidth-1 && this.right == true) || (this.headX == 0 && this.left == true)){
+		if( (this.headY == gameHeight-1 && this.down == true) || (this.headY == 0 && this.up == true) || (this.headX == gameWidth-1 && this.right == true) || (this.headX == 0 && this.left == true))
 			this.alive = false;
-		}
 
-		//Check if it itself
+		//Check if it hit itself
 		for(let i=1;i<this.snakeBody.length;i++)
-			if(this.snakeBody[i].x == this.headX && this.snakeBody[i].y == this.headY){
+			if(this.snakeBody[i].x == this.headX && this.snakeBody[i].y == this.headY)
 				this.alive = false;
-			}
 
+		//Move the snake forward in its current direction
 		if(this.alive){
 
 			if(this.up){
@@ -65,11 +77,27 @@ class snake{
 				b = new body(this.headX-1,this.headY);
 				this.headX-=1;
 			}
-			this.snakeBody.pop();
+			if(this.foodEaten == false)
+				this.snakeBody.pop();
+			else
+				this.foodEaten = false;
 			this.snakeBody.unshift(b);
 
 		}
 
+		//Check if it ate the food
+		if(this.headX == this.food.x && this.headY == this.food.y)
+			this.foodEaten = true;
+		this.positionFood();
+	}
+
+	positionFood(){
+		for(let i=0;i<this.snakeBody.length;i++){
+			if(this.food.x == this.snakeBody[i].x && this.food.y == this.snakeBody[i].y){
+				this.food = new body(floor(random(gameWidth)),floor(random(gameHeight)));
+				i=0;
+			}
+		}
 	}
 
 };
